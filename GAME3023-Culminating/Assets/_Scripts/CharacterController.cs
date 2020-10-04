569b521879a6e9823d14c255f29b31b937b151d0
RavenBP <ravenbpowless@gmail.com>
2020-10-03 23:46:19 -0400

    Added 2D unity project with rough tilemap
    - added simple 2d unity project with 3 tilemaps (base, collisions, details)
    - added simple character movement script
    - added added assets

---
+++ GAME3023-Culminating/Assets/_Scripts/CharacterController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector2 for X, Y movement
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+
+        // Translate character's transform to create movement
+        transform.Translate(movementVector);
+    }
+}
