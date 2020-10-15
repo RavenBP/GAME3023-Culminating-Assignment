f4e34e548271e2fce39d3e28784707267d0f5450
RavenBP <ravenbpowless@gmail.com>
2020-10-15 19:56:21 -0400

    added animations to player

--- GAME3023-Culminating/Assets/_Scripts/CharacterController.cs
+++ GAME3023-Culminating/Assets/_Scripts/CharacterController.cs
@@ -7,13 +7,24 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     float speed = 10;
 
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    Animator animator;
+
+    Vector2 movementVector;
+
     // Update is called once per frame
     void Update()
     {
-        // Vector2 for X, Y movement
-        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
 
-        // Translate character's transform to create movement
-        transform.Translate(movementVector);
+        rb.MovePosition(rb.position + movementVector * speed * Time.deltaTime);
+
+        animator.SetFloat("Horizontal", movementVector.x);
+        animator.SetFloat("Vertical", movementVector.y);
+        animator.SetFloat("Speed", movementVector.sqrMagnitude);
     }
 }
