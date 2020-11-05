13a9e8b00330495389a5b8c9be39945d48bcce90
RavenBP <ravenbpowless@gmail.com>
2020-11-05 12:42:35 -0500

    Added additional enemy animation clips
    - Adjusted player scale
    - Fixed LongGrass to have proper lighting

--- GAME3023-Culminating/Assets/_Scripts/EnemyController.cs
+++ GAME3023-Culminating/Assets/_Scripts/EnemyController.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
-    AnimationClip GolemAnimation;
-
-    [SerializeField]
-    AnimationClip DemonAnimation;
-
-    [SerializeField]
     Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (Random.Range(0.0f, 10.0f) > 5.0f)
+        int randomInt = Random.Range(0, 11);
+
+        // Randomly select between animation clips
+        if (randomInt >= 0 && randomInt <= 2)
+        {
+            animator.Play("Golem");
+            Debug.Log("Playing Golem Animation Clip");
+        }
+        else if (randomInt >= 3 && randomInt <= 5)
         {
             animator.Play("Demon");
             Debug.Log("Playing Demon Animation Clip");
         }
+        else if (randomInt >= 6 && randomInt <= 8)
+        {
+            animator.Play("Ogre");
+            Debug.Log("Playing Ogre Animation Clip");
+        }
         else
         {
-            animator.Play("Golem");
-            Debug.Log("Playing Golem Animation Clip");
+            animator.Play("Small Demon");
+            Debug.Log("Playing Small Demon Animation Clip");
         }
     }
 
