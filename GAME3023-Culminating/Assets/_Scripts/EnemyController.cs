c46cc0f3ebb68b93e66866a90b1edd73e1b3f1dd
RavenBP <ravenbpowless@gmail.com>
2020-11-05 12:12:09 -0500

    Added basic enemy controller
    - enemy controller will currently randomly select an animation clip

---
+++ GAME3023-Culminating/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    AnimationClip GolemAnimation;
+
+    [SerializeField]
+    AnimationClip DemonAnimation;
+
+    [SerializeField]
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.Range(0.0f, 10.0f) > 5.0f)
+        {
+            animator.Play("Demon");
+            Debug.Log("Playing Demon Animation Clip");
+        }
+        else
+        {
+            animator.Play("Golem");
+            Debug.Log("Playing Golem Animation Clip");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
