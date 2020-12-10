c08f54c374b650a66f7d2a0e334caa0294be944c
Acercam20 <cameronakey@hotmail.ca>
2020-12-10 14:56:21 -0500

    Fixed a loading bug
    Fixed a bug where the player wouldn't load the last save on starting the game.

--- GAME3023-Culminating/Assets/_Scripts/UI.cs
+++ GAME3023-Culminating/Assets/_Scripts/UI.cs
@@ -7,6 +7,13 @@ public class UI : MonoBehaviour
 {
     public void PlayGame()
     {
+        GameObject player;
+
+        if (GameObject.FindWithTag("Player") != null)
+        {
+            player = GameObject.FindWithTag("Player");
+            player.GetComponent<CharacterController>().LoadPlayer();
+        }
         SceneManager.LoadScene("GameScene");
     }
 
